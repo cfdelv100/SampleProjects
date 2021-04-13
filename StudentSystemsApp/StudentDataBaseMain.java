d05c5a2992e8f78a2dacd27b843bd09e5a19d40d
Carlos Del Valle <55850882+cfdelv100@users.noreply.github.com>
2021-04-13 14:22:18 -0500

    Create StudentDataBaseMain.java
    The main class of StudentDatabaseApp.

---
+++ StudentSystemsApp/StudentDataBaseMain.java
@@ -0,0 +1,11 @@
+package com.company;
+public class StudentDatabaseAppMain {
+    public static void main(String[]args){
+        Student student1 = new Student();
+
+        //Ask how many users we want to add
+
+        //Create a number of new students
+
+    }
+}
