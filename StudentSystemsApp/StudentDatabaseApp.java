d37fb82e628384424e016bff0809e9abdf6ab354
Carlos Del Valle <55850882+cfdelv100@users.noreply.github.com>
2021-04-19 01:29:18 -0500

    Update StudentDatabaseApp.java
    Added Generate ID and Enrolling methods

--- StudentSystemsApp/StudentDatabaseApp.java
+++ StudentSystemsApp/StudentDatabaseApp.java
@@ -11,7 +11,7 @@ public class Student {
     private String tuitionBalance;
     private int costOfCourse = 600;
     //Constructor: prompt user to enter the student's name and year
-    public Student(){
+  public StudentDatabaseApp(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the student's First Name: ");
         this.firstName = in.nextLine();
@@ -21,14 +21,37 @@ public class Student {
 
         System.out.println(" 1 - Freshmen\n 2 - Sophomore\n 3- Junior\n 4 - Senior\nEnter the student's Grade Level: ");
         this.gradeYear = in.nextInt();
-        System.out.println(firstName +  " " + lastName + " " + gradeYear);
 
+        setStudentID();
+        System.out.println(firstName +  " " + lastName + " " + gradeYear  + " " + studentID);
 
     }
 
     // Generate an ID
+    private void setStudentID(){
+        //Grade level + ID
+        id++; // adds one to the id of student
+        this.studentID = gradeYear + "" + id;
 
     //Enroll in courses
+    public void enroll(){
+        //get inside a loop, user hits 0
+       do { 
+           System.out.println("Enter a course to enroll (Q to quit): ");
+           Scanner in = new Scanner(System.in);
+           String course = in.nextLine();
+           if(!course.equals("Q")) {
+             courses = course + "\n" + course;
+             tuitionBalance = tuitionBalance + costOfCourse;
+            }
+           else{
+                break;
+            }
+         } while(1 != 0);
+           
+         System.out.print("ENROLLED IN: " + courses);
+         System.out.println("TUITION BALANCE: " + tuitionBalance);
+    }
 
     // View Balances
 
