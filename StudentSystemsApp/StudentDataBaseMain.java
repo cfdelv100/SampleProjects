588960f85e3eb6abf96870fe557a3b06f7b97652
Carlos Del Valle <55850882+cfdelv100@users.noreply.github.com>
2021-04-26 00:24:18 -0500

    Update StudentDataBaseMain.java
    Integrated main class to operate methods for an user asked amount of students to enter into the database.

--- StudentSystemsApp/StudentDataBaseMain.java
+++ StudentSystemsApp/StudentDataBaseMain.java
@@ -1,12 +1,26 @@
-package com.company;
-public class StudentDatabaseAppMain {
-    public static void main(String[]args){
-        Student student1 = new Student();
-        student1.enroll();
+import java.util.Scanner;
 
+public class StudentDatabaseMain {
+    public static void main(String[]args){
+        
         //Ask how many users we want to add
+        System.out.println("Enter the number of students you want to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        StudentDatabaseApp [] students = new StudentDatabaseApp[numOfStudents]; 
+
 
         //Create a number of new students
+        for(int i = 0; i < numOfStudents; i++){
+            students[i] = new StudentDatabaseApp();
+            students[i].enroll();
+            students[i].payBalances();
+           
+         }
+       
+        for(int i = 0; i < numOfStudents; i++){
+        System.out.println(students[i].toString());
+        }
 
     }
 }
