3683fd8c83dc4314fb9bdebd69faf1c171dcdfd7
Carlos Del Valle <55850882+cfdelv100@users.noreply.github.com>
2021-06-01 01:30:57 -0500

    Update HandTracking.py
    Added fps reading and hand drawing to image capture off webcam

--- HandTracking/HandTracking.py
+++ HandTracking/HandTracking.py
@@ -8,6 +8,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+previousTime = 0
+currentTime = 0
 while True:
     ret, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -16,7 +18,21 @@ while True:
 
     if(results.multi_hand_landmarks):
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                # if id == 0:
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    currentTime = time.time()
+    fps = 1 / (currentTime - previousTime)
+    previousTime = currentTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cv2.imshow("HandTracking", img)
     cv2.waitKey(1)
